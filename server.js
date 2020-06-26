db331d33f39ef7d490791f1f1fe07ba3fcad7d63
Ramazan Kaya <kaya-ramazan@outlook.com>
2020-06-27 02:02:02 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,19 +1,24 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
+const ShortUrl = require('./models/shortUrl')
 
-mongoose.connect('mogodb:localhost/urlShortener', {
-
+mongoose.connect('mongodb://localhost/urlShortener', {
+useNewUrlParser:true,useUnifiedTopology:true
 })
 
 app.set('view engine','ejs')
-
-app.get('/',(req,res) =>
+app.use(express.urlencoded({ extended:false }))
+app.get('/', async (req,res) =>
 {
-    res.render('index')
+    const urls = await ShortUrl.find() 
+    res.render('index',{ urls: urls })
 })
-app.post('/shortUrl',(req,res) =>
+app.post('/shortUrl', async (req,res) =>
 {
-
+   await ShortUrl.create({
+        full:req.body.fullUrl
+    })
+    res.redirect('/')
 })
 app.listen(process.env.PORT || 3000)
